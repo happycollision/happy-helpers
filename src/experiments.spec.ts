e07cfefacdcb43b222515c8c310eb77e28fb2b3c
Don Denton <don@happycollision.com>
2018-06-15 17:13:32 -0500

    refactor: group tests

--- src/experiments.spec.ts
+++ src/experiments.spec.ts
@@ -1,20 +1,22 @@
 import { RepeatableEvent } from './experiments';
 
 describe('RepeatableEvent', () => {
-  it('constructs with a starting date string', () => {
-    expect(new RepeatableEvent('2001-01-01').startingDate).toEqual(new Date('2001-01-01'))
-  })
-
-  it('disallows strings other than yyyy-mm-dd', () => {
-    expect(() => new RepeatableEvent('01-01').startingDate).toThrowError();
-  })
-
-  it('catches the simplest mistakes about the date string', () => {
-    expect(() => new RepeatableEvent('2001-13-01').startingDate).toThrowError('13');
-    expect(() => new RepeatableEvent('2001-10-32').startingDate).toThrowError('32');
-  })
-
-  it('constructs with a label', () => {
-    expect(new RepeatableEvent('2001-01-01', 'My Event').label).toEqual('My Event');
+  describe('instantiation', () => {
+    it('constructs with a starting date string', () => {
+      expect(new RepeatableEvent('2001-01-01').startingDate).toEqual(new Date('2001-01-01'))
+    })
+  
+    it('disallows strings other than yyyy-mm-dd', () => {
+      expect(() => new RepeatableEvent('01-01').startingDate).toThrowError();
+    })
+  
+    it('catches the simplest mistakes about the date string', () => {
+      expect(() => new RepeatableEvent('2001-13-01').startingDate).toThrowError('13');
+      expect(() => new RepeatableEvent('2001-10-32').startingDate).toThrowError('32');
+    })
+  
+    it('constructs with a label', () => {
+      expect(new RepeatableEvent('2001-01-01', 'My Event').label).toEqual('My Event');
+    })
   })
 });
