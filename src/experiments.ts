e38a6e5eb0c3820f03803e5c262d783bdd0a5ec3
Don Denton <don@happycollision.com>
2018-06-15 17:07:10 -0500

    create constructor that takes a date

---
+++ src/experiments.ts
@@ -0,0 +1,33 @@
+
+
+export class RepeatableEvent {
+  startingDate: Date;
+
+  constructor(startingDate: string | Date) {
+    this.validateConstructorInput(startingDate);
+    this.startingDate = new Date(startingDate);
+  }
+
+  private validateConstructorInput(startingDate): void {
+    if (startingDate instanceof Date) return;
+    const baseErrorMessage = 'The constructor for RepeatableEvent will only take a string with the format `yyyy-mm-dd`.'
+    let errorMessage: string; 
+    const regex = /([\d]{4})-([\d]{2})-([\d]{2})/
+    if (!regex.test(startingDate)) {
+      throw new Error(baseErrorMessage)
+    }
+    const [_, year, month, day] = startingDate.match(regex).map((part, i) => {
+      if (i === 0) return part;
+      return parseInt(part, 10);
+    });
+    if (month > 12 || month < 1) {
+      errorMessage = `${errorMessage} The month, which should be 1 through 12, was given as ${month}.`
+    }
+    if (day > 31 || day < 1) {
+      errorMessage = `${errorMessage} The day, which should be 1 through 31, was given as ${day}.`
+    }
+    if (errorMessage) {
+      throw new Error(`${baseErrorMessage} ${errorMessage}`)
+    }
+  }
+}
